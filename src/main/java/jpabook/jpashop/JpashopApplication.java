4160ed8fc87a13269fe9d407752dd3e639acf3b0
hyse <gustmd4296@hanmail.net>
2022-06-29 11:49:01 +0900

    ㅇㄹㄹㄴㅇ

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -10,7 +10,7 @@ public class JpashopApplication {
 	public static void main(String[] args) {
 
 
-		SpringApplication.run(JpashopApplication.class, args);
+		SpringApplication.run(JpashopApplication.class, args);ㅇㅁㄹ
 	}
 
 }
